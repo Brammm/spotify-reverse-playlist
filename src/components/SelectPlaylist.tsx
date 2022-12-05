33a2eec883a5ce1d978e67a2b23cd48f1adc7807
Bram Van der Sype <bram.vandersype@gmail.com>
2022-12-05 10:17:45 +0100

    Reverse the playlist

--- src/components/SelectPlaylist.tsx
+++ src/components/SelectPlaylist.tsx
@@ -10,6 +10,11 @@ type Playlist = {
     name: string;
 }
 
+type Track = {
+    id: string;
+    uri: string;
+}
+
 const SelectPlaylist = ({token}: Props) => {
     const [playlists, setPlaylists] = useState<Playlist[]>([]);
 
@@ -27,12 +32,46 @@ const SelectPlaylist = ({token}: Props) => {
         })();
     }, []);
 
+    const reversePlaylist = async (id: string) => {
+        // Get selected playlist info and tracks in chunks
+        let {data: {public: publicPlaylist, name, tracks: {next: url, items}}}: any = await axios.get(
+            'https://api.spotify.com/v1/playlists/' + id,
+            {headers: {authorization: 'Bearer ' + token}});
+        let chunks = [items.map((item: { track: Track }) => item.track.uri)];
+        while (url) {
+            let {data}: any = await axios.get(url, {headers: {authorization: 'Bearer ' + token}});
+            url = data.next;
+            chunks.push(data.items.map((item: { track: Track }) => item.track.uri));
+        }
+
+        // Create new playlist
+        const {data: newPlaylist} = await axios.post('https://api.spotify.com/v1/me/playlists', {
+            name: name + ' - Reversed',
+            public: publicPlaylist,
+        }, {headers: {authorization: 'Bearer ' + token}});
+
+        // Add tracks in reverse order
+        for (let i = chunks.length - 1; i >= 0; i--) {
+            const reversed = chunks[i].reverse();
+
+            await axios.post(
+                `https://api.spotify.com/v1/playlists/${newPlaylist.id}/tracks`,
+                {uris: reversed},
+                {headers: {authorization: 'Bearer ' + token}}
+            );
+        }
+
+        alert('Reversed playlist');
+    };
+
     return (
         <div>
             <h1>Select playlist</h1>
             <ul>
                 {playlists.map(playlist => (
-                    <li key={playlist.id}>{playlist.name}</li>
+                    <li key={playlist.id}>
+                        <button onClick={() => reversePlaylist(playlist.id)}>{playlist.name}</button>
+                    </li>
                 ))}
             </ul>
         </div>
