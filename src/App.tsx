cc901773699407cfbcc2b4c2c36bef4ec0717792
Bram Van der Sype <bram.vandersype@gmail.com>
2022-12-05 11:40:32 +0100

    Handle errors when reversing playlist

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,6 @@ const App = () => {
     });
 
     const handleLogin = (token: string) => {
-        // TODO: handle token expiry
         setAppState({status: 'authenticated', token});
         window.sessionStorage.setItem('token', token);
         window.history.replaceState({}, '', window.location.origin);
@@ -28,11 +27,18 @@ const App = () => {
         window.history.replaceState({}, '', window.location.origin);
     };
 
+    const logout = () => {
+        window.sessionStorage.removeItem('token');
+        window.sessionStorage.removeItem('state');
+        window.sessionStorage.removeItem('codeVerifier');
+        setAppState({status: 'unauthenticated'})
+    }
+
     switch (appState.status) {
         case 'unauthenticated':
             return <Login onLogin={handleLogin} onError={handleError} error={appState.error} />;
         case 'authenticated':
-            return <SelectPlaylist token={appState.token} />;
+            return <SelectPlaylist token={appState.token} logout={logout} />;
     }
 };
 
--- src/components/SelectPlaylist.tsx
+++ src/components/SelectPlaylist.tsx
@@ -4,9 +4,10 @@ import Layout from './Layout';
 
 type Props = {
     token: string;
+    logout: () => void;
 }
 
-type Status = 'loading' | 'loaded' | 'reversing' | 'success';
+type Status = 'loading' | 'loaded' | 'reversing' | 'success' | 'error';
 
 type Playlist = {
     id: string;
@@ -18,62 +19,71 @@ type Track = {
     uri: string;
 }
 
-const SelectPlaylist = ({token}: Props) => {
+const SelectPlaylist = ({token, logout}: Props) => {
     const [playlists, setPlaylists] = useState<Playlist[]>([]);
     const [status, setStatus] = useState<Status>('loading');
 
     useEffect(() => {
         (async () => {
-            let url = 'https://api.spotify.com/v1/me/playlists';
-            let playlists: Playlist[] = [];
-            while (url) {
-                const {data: {items, next}} = await axios.get(url, {headers: {authorization: 'Bearer ' + token}});
-                playlists = playlists.concat(items);
-                url = next;
-            }
+            try {
+                let url = 'https://api.spotify.com/v1/me/playlists';
+                let playlists: Playlist[] = [];
+                while (url) {
+                        const {data: {items, next}} = await axios.get(url, {headers: {authorization: 'Bearer ' + token}});
+                        playlists = playlists.concat(items);
+                        url = next;
+                }
 
-            setPlaylists(playlists);
-            setStatus('loaded')
+                setPlaylists(playlists);
+                setStatus('loaded')
+            } catch {
+                setStatus('error');
+            }
         })();
     }, []);
 
     const reversePlaylist = async (id: string) => {
         setStatus('reversing');
-        // Get selected playlist info and tracks in chunks
-        let {data: {public: publicPlaylist, name, tracks: {next: url, items}}}: any = await axios.get(
-            'https://api.spotify.com/v1/playlists/' + id,
-            {headers: {authorization: 'Bearer ' + token}});
-        let chunks = [items.map((item: { track: Track }) => item.track.uri)];
-        while (url) {
-            let {data}: any = await axios.get(url, {headers: {authorization: 'Bearer ' + token}});
-            url = data.next;
-            chunks.push(data.items.map((item: { track: Track }) => item.track.uri));
+        try {
+            // Get selected playlist info and tracks in chunks
+            let {data: {public: publicPlaylist, name, tracks: {next: url, items}}}: any = await axios.get(
+                'https://api.spotify.com/v1/playlists/' + id,
+                {headers: {authorization: 'Bearer ' + token}});
+            let chunks = [items.map((item: { track: Track }) => item.track.uri)];
+            while (url) {
+                let {data}: any = await axios.get(url, {headers: {authorization: 'Bearer ' + token}});
+                url = data.next;
+                chunks.push(data.items.map((item: { track: Track }) => item.track.uri));
+            }
+
+            // Create new playlist
+            const {data: newPlaylist} = await axios.post('https://api.spotify.com/v1/me/playlists', {
+                name: name + ' - Reversed',
+                public: publicPlaylist,
+            }, {headers: {authorization: 'Bearer ' + token}});
+
+            // Add tracks in reverse order
+            for (let i = chunks.length - 1; i >= 0; i--) {
+                const reversed = chunks[i].reverse();
+
+                await axios.post(
+                    `https://api.spotify.com/v1/playlists/${newPlaylist.id}/tracks`,
+                    {uris: reversed},
+                    {headers: {authorization: 'Bearer ' + token}}
+                );
+            }
+            setStatus('success');
+        } catch {
+            setStatus('error');
         }
-
-        // Create new playlist
-        const {data: newPlaylist} = await axios.post('https://api.spotify.com/v1/me/playlists', {
-            name: name + ' - Reversed',
-            public: publicPlaylist,
-        }, {headers: {authorization: 'Bearer ' + token}});
-
-        // Add tracks in reverse order
-        for (let i = chunks.length - 1; i >= 0; i--) {
-            const reversed = chunks[i].reverse();
-
-            await axios.post(
-                `https://api.spotify.com/v1/playlists/${newPlaylist.id}/tracks`,
-                {uris: reversed},
-                {headers: {authorization: 'Bearer ' + token}}
-            );
-        }
-        setStatus('success');
     };
 
     return (
         <Layout>
             <p className="mb-4">Select the playlist you wish to revert. A new playlist will be created with the same name and have " - Reversed" appended to it. </p>
             <p className="mb-4">If the selected playlist was set as private, the reversed one will be created as private as well.</p>
-            {status === 'loading' && <p>Loading ...</p>}
+            {status === 'error' && <p>Something went wrong. Please try <button className="text-white hover:underline" onClick={logout}>logging in</button> again.</p>}
+            {status === 'loading' && <p>Loading&hellip;</p>}
             {status === 'loaded' && (
                 <ul>
                     {playlists.map(playlist => (
@@ -83,7 +93,7 @@ const SelectPlaylist = ({token}: Props) => {
                     ))}
                 </ul>
             )}
-            {status === 'reversing' && <p>Creating reversed playlist ...</p>}
+            {status === 'reversing' && <p>Creating reversed playlist&hellip;</p>}
             {status === 'success' && <p>Done!</p>}
         </Layout>
     );
